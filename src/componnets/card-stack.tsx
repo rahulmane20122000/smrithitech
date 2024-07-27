8a12bda31f34ffbb0f5ebc3b923b40e851fcea57
abdulrahilsheikh <rahilsheikh67064@gmail.com>
2024-07-27 23:05:38 +0530

    adding cards

---
+++ src/componnets/card-stack.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+let interval: any;
+
+type Card = any;
+
+export const CardStack = ({
+  items,
+  offset,
+  scaleFactor,
+}: {
+  items: any[];
+  offset?: number;
+  scaleFactor?: number;
+}) => {
+  const CARD_OFFSET = offset || 10;
+  const SCALE_FACTOR = scaleFactor || 0.06;
+  const [cards, setCards] = useState<Card[]>(items);
+
+  useEffect(() => {
+    startFlipping();
+
+    return () => clearInterval(interval);
+  }, []);
+  const startFlipping = () => {
+    interval = setInterval(() => {
+      setCards((prevCards: Card[]) => {
+        const newArray = [...prevCards]; // create a copy of the array
+        newArray.unshift(newArray.pop()!); // move the last element to the front
+        return newArray;
+      });
+    }, 5000);
+  };
+
+  return (
+    <div className="relative">
+      {cards.map((card, index) => {
+        return (
+          <motion.div
+            key={card.id}
+            className="absolute  bg-white  rounded shadow-lg border border-neutral-200  shadow-black/[0.1] flex flex-col justify-between"
+            style={{
+              transformOrigin: "top center",
+            }}
+            animate={{
+              top: index * -CARD_OFFSET,
+              scale: 1 - index * SCALE_FACTOR, // decrease scale for cards that are behind
+              zIndex: cards.length - index, //  decrease z-index for the cards that are behind
+            }}
+          >
+            {card.content}
+          </motion.div>
+        );
+      })}
+    </div>
+  );
+};
--- src/sections/ed-tech-services/ed-tech-services.tsx
+++ src/sections/ed-tech-services/ed-tech-services.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Accordion from "../../componnets/accordian/accordian";
-import { BorderBeam } from "../../componnets/border-beam/border-beam";
+import { CardStack } from "../../componnets/card-stack";
 
 const content = [
   {
@@ -8,7 +8,7 @@ const content = [
     content:
       "Get hands-on experience with data science course including Python, Machine Learning, Deep Learning, Natural Language Processing (NLP), MLOps, Recurrent Neural Networks (RNNs), Generative Adversarial Networks (GANs), Attention, Transformers, BERT, and Business Intelligence Tools.",
     image:
-      "https://plus.unsplash.com/premium_photo-1683121710572-7723bd2e235d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3DA",
+      "https://images.unsplash.com/photo-1697577418970-95d99b5a55cf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXJ0aWZpY2lhbCUyMGludGVsbGlnZW5jZXxlbnwwfHwwfHx8MA%3D%3D",
   },
   {
     title: "Cyber Security & Ethical Hacking",
@@ -77,14 +77,22 @@ const EdTechServices = () => {
           </Accordion>
         ))}
       </div>
-      <div className="flex items-center justify-center p-10">
-        <div className="relative w-56 h-full rounded">
-          <img
+      <div className="flex items-start justify-center p-10">
+        {/* <img
             className="p-1 w-full h-full object-cover rounded"
             src={content[openAccordian].image}
-          />
-          <BorderBeam size={250} duration={12} delay={9} />
-        </div>
+          /> */}
+        <CardStack
+          items={content.map((i) => ({
+            id: i.title,
+            content: (
+              <div className="w-64 h-96 rounded m-1 overflow-hidden">
+                <img className="w-full h-full object-cover" src={i.image} />
+              </div>
+            ),
+          }))}
+        />
+        {/* <BorderBeam size={250} duration={12} delay={9} /> */}
       </div>
     </div>
   );
